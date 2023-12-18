0648aed93266ac9163545e9cde0878fc196c3c87
Harrison <Harrison.couto@outlook.com>
2023-12-18 20:43:20 -0300

    Feat: criando mock de projetos e iniciando a estilizaçao da pagina de projetos

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { mockProjects } from '@/mock/data'
+import Image from 'next/image'
+import styles from '../../styles/projects/project.module.css'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+      <h1 className={styles.title}>Projetos</h1>
+      {mockProjects.map((project) => (
+          <div className={styles.container} key={project.id}>
+            <p>{project.name}</p>
+            <Image src={project.image} width='200' height='200' alt='img' />
+            <ol>{project.tech.map((tech, index) => (
+              <li key={index}>{tech}</li>
+            ))}</ol>
+            <p></p>
+          </div>
+      ))}
+    </div>
   )
 }
+/*       id: '1',
+      name: 'Pixel Art',
+      deploy: 'https://github.com/HarrisonSilva',
+      image: false,
+      type: 'DOM',
+      tech: ['JavaScrip, CSS, HTML'],
+      details: `molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum`,
+      repositorio: 'https://github.com/HarrisonSilva', */
