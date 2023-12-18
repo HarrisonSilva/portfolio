789df76326eaf579454e58a4219afc597698f7fe
Harrison <Harrison.couto@outlook.com>
2023-12-18 18:56:02 -0300

    Feat: criando a a pagina de sobre

--- src/app/sobre/page.jsx
+++ src/app/sobre/page.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styles from '../../styles/about/about.module.css'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>
+        <h1 className={styles.title}>Sobre mim</h1>
+        <p className={styles.paragraph}>
+    Desde muito jovem, sempre fui fascinado em resolver pequenos problemas <br />
+    como abrir meu próprio vídeo game para solucionar os problemas <br />
+    que ele apresentava Mesmo sem conhecimento na época, minha curiosidade<br />
+    fascinação e, claro a ideia do vídeo game voltar a funcionar faziam <br />
+    com que eu conseguisse solucionar o problema Essa ideia de querer <br />
+    resolver problemas foi se expandindo até que, de fato encontrei uma área <br />
+    que faz todo sentido para essa fascinação por resolver problemas<br />
+    que é a área da programação. Sem contar que ainda posso usar<br />
+    minhas habilidades de design gráfico junto com a programação.
+        </p>
+    </div>
   )
 }
