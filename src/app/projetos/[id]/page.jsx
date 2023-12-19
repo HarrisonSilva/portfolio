12dd1675a0ec9540107994fa73185423412219b1
Harrison <Harrison.couto@outlook.com>
2023-12-18 22:32:16 -0300

    Feat: termimando layout da pagina de projetos e iniciando o layout sobre o projeto (id)

---
+++ src/app/projetos/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../../styles/projects/projectId.module.css'
+
+export default function page() {
+  return (
+    <div>
+        <h1 className={styles.title}>projeto</h1>
+    </div>
+  )
+}
--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -2,21 +2,28 @@ import React from 'react'
 import { mockProjects } from '@/mock/data'
 import Image from 'next/image'
 import styles from '../../styles/projects/project.module.css'
+import Link from 'next/link'
 
 export default function page() {
   return (
     <div>
       <h1 className={styles.title}>Projetos</h1>
+      <div className={styles.container}>
       {mockProjects.map((project) => (
-          <div className={styles.container} key={project.id}>
-            <p>{project.name}</p>
-            <Image src={project.image} width='200' height='200' alt='img' />
-            <ol>{project.tech.map((tech, index) => (
+        
+        <div key={project.id} className={styles.card} >
+            <h2 className={styles.name}>{project.name}</h2>
+            <Link href={`/projetos/${project.id}`}>
+            <Image as="image" priority src={project.image} width='300' height='300' alt='img' />
+            </Link>
+            <ul className={styles.list}>{project.tech.map((tech, index) => (
               <li key={index}>{tech}</li>
-            ))}</ol>
+              ))}</ul>
             <p></p>
           </div>
+    
       ))}
+      </div>
     </div>
   )
 }
