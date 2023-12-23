fadd7403ca4fdc62703e8e777e54ed85d37bdcad
Harrison <Harrison.couto@outlook.com>
2023-12-23 02:12:29 -0300

    ajustando css

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '../styles/notfound/notfound.module.css'
-import FTR from '@/components'
 
 export default function NotFound() {
   return (
@@ -8,7 +7,6 @@ export default function NotFound() {
         <h1 className={styles.title}>
         essa página <span className={styles.notfound}>não existe!</span>
         </h1>
-        <FTR />
     </div>
   )
 }
