1bf8a47e27b037877f70b0b964b456a9f8f76284
Harrison <Harrison.couto@outlook.com>
2023-12-26 19:23:45 -0300

    Atualizando layout

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,14 @@ import Link from 'next/link'
 export default function page() {
   return (
     <div>
-      <Image priority className={styles.photo} src='/images/profile/myphoto.png' width='200' height='200' alt='minha-foto'/>
       <div className={styles.container}>
-        <h3 className={styles.hello}>Ola Mundo,</h3>
-        <h1 className={styles.title}>Eu sou <span className={styles.name}>Harrison</span></h1>
-        <h4 className={styles.professional}>Desenvolvedor Web FullStack</h4>
-      </div>
-      <div>
-
-      <p className={styles.paragraph}>Apaixonado em Resolver resoluçoes de problemas <br/> sempre em busca da melhor soluçao</p>
+     
+      <Image priority className={styles.photo} src='/images/profile/myphoto.png' width='200' height='200' alt='minha-foto'/>
+          <div className={styles.containerData}>
+            <h3 className={styles.hello}>Ola Mundo,</h3>
+            <h1 className={styles.title}>Eu sou <span className={styles.name}>Harrison</span></h1>
+            <h4 className={styles.professional}>Desenvolvedor Web FullStack</h4>
+          </div>
       </div>
       <div className={styles.containerInfo}>
         <Link className={styles.cv} href='https://drive.google.com/file/d/1JXyWQA6NfY969q5oljjKVbTrxfWgKJll/view?usp=sharing' target='_blank'><h3>Acessar CV</h3></Link>
