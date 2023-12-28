b6e0cbadcf748df647b3f57f23788a5a1fe1024b
Harrison <Harrison.couto@outlook.com>
2023-12-27 23:37:20 -0300

    adicionando linha animada

--- src/app/page.js
+++ src/app/page.js
@@ -16,6 +16,7 @@ export default function page() {
             <h4 className={styles.professional}>Desenvolvedor Web FullStack</h4>
           </div>
       </div>
+      <hr className={styles.line}/>
       <div className={styles.containerInfo}>
         <Link className={styles.cv} href='https://drive.google.com/file/d/1JXyWQA6NfY969q5oljjKVbTrxfWgKJll/view?usp=sharing' target='_blank'><h3>Acessar CV</h3></Link>
         <h3 className={styles.email}>E-mail: Harrison.couto@outlook.com</h3>
