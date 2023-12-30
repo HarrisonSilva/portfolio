3f7fd1126d343d010fa272e9ae31b3d8a6f778b8
Harrison <Harrison.couto@outlook.com>
2023-12-30 01:23:56 -0300

    diminuindo o tamanho do detalhes do projeto credit next

--- src/projects/data.js
+++ src/projects/data.js
@@ -106,11 +106,9 @@ export const projectsData = [
       image: creditNextImg,
       type: 'Frontend',
       tech: ['NextJs', 'CSS Modules'],
-      details: `Credit Next e uma experiençia aonde voce pode cadastrar seus cartões de credito virtual colocando suas informaçoes
-                de forma intuitiva, a aplicaçao funciona de forma em que voce possa vizulizar de forma simulada como ficaria seu cartao de credito
-                no banco escolhido. tambem foi criado uma aba (pagina) para voce poder vizualizar seus cartoes de creditos criados
-                abaixo de cada cartão criado existe botao de deletar o cartao caso seja necessario a exclusão do mesmo
-                existe tambem uma aba contendo informaçoes importante para voce saber qual banco escolher, contendo seus digitos iniciais.`,
+      details: `O "Credit Next" simplifica o cadastro de cartões de crédito virtual com simulações visuais, oferecendo uma página para gerenciar e excluir cartões
+                Fornece também informações úteis sobre diferentes bancos para facilitar a escolha dos usuários. 
+                Resumindo, é uma solução intuitiva para gestão de cartões de crédito virtuais.`,
       repositorio: 'https://github.com/HarrisonSilva/creditnext',
     },
    /*  {
