93560110d8666fb37929226e7afd2d34920ab593
Harrison <Harrison.couto@outlook.com>
2024-01-17 21:50:41 -0300

    mexendo no css do header e do footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,9 @@ import styles from '../../styles/footer/footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-        Desenvolvido por Harrison Couto &copy; 2023
+      <footer className={styles.footer}>
+        <h4>Copyright &copy; 2023 HC. Todos os direitos reservados</h4>
+        <p>Desenvolvido por Harrison Couto</p>
       </footer>
   )
 }
